ac89b73497fbdbbc0337c8e1fbff77e5df539818
Jameel Fattini <fatanij@student.swosu.edu>
2015-10-19 14:55:01 -0500

    have the basic chinese zodiac working

--- src/project2.java
+++ src/project2.java
@@ -1,8 +1,28 @@
-
+import java.util.Scanner;
 public class project2 {
-
 	public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the year you were born: ");
+		int year = input.nextInt();
+		
+		System.out.print("\nYou were born in the year of the ");
+		
+		switch (year % 12){
+		case 0: System.out.println("monkey."); break;
+		case 1: System.out.println("rooster."); break;
+		case 2: System.out.println("dog."); break;
+		case 3: System.out.println("pig."); break;
+		case 4: System.out.println("rat."); break;
+		case 5: System.out.println("ox."); break;
+		case 6: System.out.println("tiger."); break;
+		case 7: System.out.println("rabbit."); break;
+		case 8: System.out.println("dragon."); break;
+		case 9: System.out.println("snake."); break;
+		case 10: System.out.println("horse."); break;
+		case 11: System.out.println("sheep."); 
+		}
+		
 		
 	}
-
 }
